3601ba3ea50c1250a7f7eb4b158a7bf6a0fe7ba6
L.Joel <leopard10000@gmail.com>
2016-10-16 23:55:44 +0900

    mLab Mongodbへの接続テスト

--- spec/helpers/battles_helper_spec.rb
+++ spec/helpers/battles_helper_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe BattlesHelper, type: :helper do
     expect(helper.convert_reserved_datetime_string(dt)).to eq "1月21日 03:04"
 
   end
+
+
+  it "Access MongoDB on Heroku" do
+    puts Gem.loaded_specs["mongo"].version
+
+    mongo_uri = ENV['MONGODB_URI']
+    client = Mongo::Client.new(mongo_uri)
+    db = client.database
+    db.collection_names.each{|name| puts name }
+
+    collection = client[:battles]
+    result = collection.insert_one({code: 'ttt', opponent_name: 'ちょー', base_count: 10})
+    p result
+    expect(result.n).to eq 1  # insertした件数
+
+  end
  end
