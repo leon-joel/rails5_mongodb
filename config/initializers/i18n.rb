c054a9ba5e56b266d2654a8c892e88105dcc2a10
L.Joel <leopard10000@gmail.com>
2016-10-28 00:42:03 +0900

    エラーが発生した時にちゃんとメッセージが表示されないので、i18nは当分おあずけ

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,5 +2,5 @@
 # Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
 # Rails.application.config.i18n.default_locale = :ja
 
-I18n.enforce_available_locales = false    # http://blog.n-z.jp/blog/2013-12-04-rails-i18n-deprecated-warning.html
-Rails.application.config.i18n.default_locale = :ja
+# I18n.enforce_available_locales = false    # http://blog.n-z.jp/blog/2013-12-04-rails-i18n-deprecated-warning.html
+# Rails.application.config.i18n.default_locale = :ja
